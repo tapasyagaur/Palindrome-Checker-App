64dc560398c69732c2e55042cb6f41133d9aa9b2
tapasyagaur <its.tapasyagaur@gmail.com>
2026-03-11 14:53:39 +0530

    Added UC2 Hardcoded Palindrome Checker

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,22 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = "";
+
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+
+        // Check if palindrome
+        if (word.equalsIgnoreCase(reversed)) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
